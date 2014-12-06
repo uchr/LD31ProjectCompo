8b75ee6cebd209ff445f7dd546eb67e742e1e666
Anton Tarasov <uchiro.me@gmail.com>
2014-12-06 21:46:32 +0600

    Add island

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private Transform cachedTransform;
+	private Transform cachedInner;
+
+	public float speed = 15.0f;
+
+	private void Awake () {
+		cachedTransform = transform;
+		cachedInner = GameObject.Find("Player/Inner").GetComponent<Transform>();
+	}
+
+	private void Update () {
+		Vector3 direct = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+		if (direct.magnitude != 0.0f)
+			cachedTransform.position += direct * Time.deltaTime * speed;
+
+		RaycastHit hit;
+		Ray ray =  new Ray(cachedTransform.position, -Vector3.up);
+		//Debug.DrawLine(ray.origin, ray.direction * 500.0f + ray.origin, Color.green);
+		var layerMask = 1 << 8;
+		if (Physics.Raycast(ray, out hit, 500.0f, layerMask)) {
+			Vector3 position = new Vector3(cachedTransform.position.x, hit.point.y + cachedTransform.localScale.y, cachedTransform.position.z);
+			//Debug.DrawLine(ray.origin, hit.point);
+			transform.position = position;
+		}
+	}
+}
---
+++ Assets/Scripts/RotateIsland.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateIsland : MonoBehaviour {
+
+	private Transform cachedTransform;
+	private Transform player;
+
+	public float speed = 10.0f;
+
+	private void Awake () {
+		cachedTransform = transform;
+		player = GameObject.Find("Player").GetComponent<Transform>();
+	}
+
+	private void Update () {
+		float angle = Vector3.Angle(Vector3.back + cachedTransform.position, player.position - cachedTransform.position);
+		//Debug.DrawLine(cachedTransform.position, Vector3.back * 500.0f + cachedTransform.position, Color.green);
+		//Debug.DrawLine(cachedTransform.position, (player.position - cachedTransform.position) * 500.0f + cachedTransform.position, Color.green);
+		//Debug.Log("X: " + player.position.x);
+		//Debug.Log(angle * Mathf.Sign(player.position.x));
+		if (angle * Mathf.Sign(player.position.x) > 10.0f) {
+			cachedTransform.Rotate(Vector3.up * Time.deltaTime * Mathf.Lerp(0.0f, speed, (angle - 10.0f)) / 5.0f);
+		}
+		if (angle * Mathf.Sign(player.position.x) < 10.0f) {
+			cachedTransform.Rotate(-Vector3.up * Time.deltaTime * Mathf.Lerp(0.0f, speed, (angle - 10.0f)) / 5.0f);
+		}
+	}
+}
