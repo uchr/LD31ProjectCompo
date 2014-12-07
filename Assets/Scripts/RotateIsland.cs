c140695b91281c8f30bd019c226a785dd42be1e3
Anton Tarasov <uchiro.me@gmail.com>
2014-12-08 04:53:53 +0600

    Clear debug

--- Assets/Scripts/RotateIsland.cs
+++ Assets/Scripts/RotateIsland.cs
@@ -16,13 +16,13 @@ public class RotateIsland : MonoBehaviour {
 	private void Update () {
 		Vector3 dirToPlayer = player.position - cachedTransform.position;
 		dirToPlayer.y = 0.0f;
-		//dirToPlayer = dirToPlayer.normalized;
+		// dirToPlayer = dirToPlayer.normalized;
 		float angle = Vector3.Angle(Vector3.back, dirToPlayer);
 		// Debug.DrawLine(cachedTransform.position, Vector3.back * 500.0f + cachedTransform.position, Color.white);
 		// Debug.DrawLine(cachedTransform.position, dirToPlayer * 500.0f + cachedTransform.position, Color.green);
 		// Debug.Log("X: " + player.position.x);
-		Debug.Log(angle * Mathf.Sign(player.position.x));
-		//Debug.Log(angle);
+		// Debug.Log(angle * Mathf.Sign(player.position.x));
+		// Debug.Log(angle);
 		if (angle * Mathf.Sign(player.position.x) > 5.0f) {
 			// cachedTransform.forward = Vector3.Lerp(cachedTransform.forward, -dirToPlayer, Time.deltaTime * speed);
 			cachedTransform.Rotate(Vector3.up * Time.deltaTime * Mathf.Lerp(0.0f, speed, (angle - 5.0f)) / 5.0f);
