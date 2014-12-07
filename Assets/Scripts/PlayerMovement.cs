1b84db07a4388b843c5d01c8901e9046970af811
Anton Tarasov <uchiro.me@gmail.com>
2014-12-07 22:58:23 +0600

    Add player move and more

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ public class PlayerMovement : MonoBehaviour {
 
 	private Transform cachedTransform;
 	private Transform cachedInner;
+	public Animator playerAnimator;
 
 	public float speed = 15.0f;
 
@@ -15,6 +16,10 @@ public class PlayerMovement : MonoBehaviour {
 
 	private void Update () {
 		Vector3 direct = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+		if (direct.magnitude > 0.5f)
+			cachedInner.right = direct.normalized;
+		rigidbody.velocity = Vector3.zero;
+		playerAnimator.SetFloat("Velocity", direct.magnitude);
 		if (direct.magnitude != 0.0f)
 			cachedTransform.position += direct * Time.deltaTime * speed;
 
@@ -23,7 +28,7 @@ public class PlayerMovement : MonoBehaviour {
 		//Debug.DrawLine(ray.origin, ray.direction * 500.0f + ray.origin, Color.green);
 		var layerMask = 1 << 8;
 		if (Physics.Raycast(ray, out hit, 500.0f, layerMask)) {
-			Vector3 position = new Vector3(cachedTransform.position.x, hit.point.y + cachedTransform.localScale.y, cachedTransform.position.z);
+			Vector3 position = new Vector3(cachedTransform.position.x, hit.point.y + cachedTransform.localScale.y * 3.0f, cachedTransform.position.z);
 			//Debug.DrawLine(ray.origin, hit.point);
 			transform.position = position;
 		}
