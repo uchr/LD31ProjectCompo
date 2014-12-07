de3127f7b70631ea957fcb7ca6735fb23de5387a
Anton Tarasov <uchiro.me@gmail.com>
2014-12-08 04:52:50 +0600

    Fix rotate camera

--- Assets/Scripts/MainQuest.cs
+++ Assets/Scripts/MainQuest.cs
@@ -10,26 +10,14 @@ public class MainQuest : MonoBehaviour {
 	public bool isWin = false;
 	public bool isLose = false;
 
+	public GameObject dialogBox;
 	public Text textDialog;
-	public string [] message = new string[]{
-					@"Hi, I am very scared. And I really need your help… 
-					I was running away from them… But along the way I 
-					lost my medallion. It is my last chance to stay alive.. 
-					But I sprained my leg… Help me to find my medallion. 
-					Please. I was running from that side of the island…",
-					@"You really think that I will tell you something about 
-					it? But I might give you some information, if you bring 
-					me food, bottle of rum and gunpowder. And you will shoot 
-					from the gun.",
-					@"Why should I help you? I’m not sure that I want to help 
-					you. But you can try. Bring me the core of the First Kraken. 
-					And I will think about your situation.",
-					@"Yeah, I have heard about it. And I will glad to help you. 
-					However, can you help me, too? Could you bring me the letter 
-					from the Second Man?",
-					@"You are in a very unpleasant situation, man. If I were you, 
-					I would escape from this island as soon as possible! Very 
-					strange things are happening here! Leave! Save your life!"
+	private string [] message = new string[]{
+					"Hi, I am very scared. And I really need your help… I was running away from them… But along the way I lost my medallion. It is my last chance to stay alive.. But I sprained my leg… Help me to find my medallion. Please. I was running from that side of the island…",
+					"You really think that I will tell you something about it? But I might give you some information, if you bring me food, bottle of rum and gunpowder. And you will shoot from the gun.",
+					"Why should I help you? I’m not sure that I want to help you. But you can try. Bring me the core of the First Kraken. And I will think about your situation.",
+					"Yeah, I have heard about it. And I will glad to help you. However, can you help me, too? Could you bring me the letter from the Second Man?",
+					"You are in a very unpleasant situation, man. If I were you, I would escape from this island as soon as possible! Very strange things are happening here! Leave! Save your life!"
 				};
 
 	public GameObject [] parts;
@@ -40,6 +28,7 @@ public class MainQuest : MonoBehaviour {
 	private float timer;
 
 	public void ShowMessage() {
+		dialogBox.SetActive(true);
 		textDialog.text = GetMessage();
 		timer = time;
 	}
@@ -66,25 +55,28 @@ public class MainQuest : MonoBehaviour {
 	//curPart = parts[stage].GetComponent<PartOfQuest>();
 	public void NextStage(int fromStage) {
 		parts[fromStage].SetActive(false);
-		++stage;
-		parts[stage].SetActive(true);
-
 		if (stage == 0 || stage == 1 || stage == 6 || stage == 9 || stage == 14)
 			ShowMessage();
+
+		++stage;
+		parts[stage].SetActive(true);
 	}
 
 	private void Awake () {
 		instance = this;
-		for (int i = 0; i < parts.Length; ++i)
+		for (int i = 1; i < parts.Length; ++i)
 			parts[i].SetActive(false);
 	}
 
 	private void Update () {
+		if (Input.GetButtonDown("Jump"))
+			ShowMessage();
+			
 		if (timer >= 0.0f) {
 			timer -= Time.deltaTime;
 		}
 		if (timer < 0.0f) {
-
+			dialogBox.SetActive(false);
 		}
 	}
 }
--- Assets/Scripts/RotateIsland.cs
+++ Assets/Scripts/RotateIsland.cs
@@ -14,16 +14,21 @@ public class RotateIsland : MonoBehaviour {
 	}
 
 	private void Update () {
-		float angle = Vector3.Angle(Vector3.back + cachedTransform.position, player.position - cachedTransform.position);
-		//Debug.DrawLine(cachedTransform.position, Vector3.back * 500.0f + cachedTransform.position, Color.green);
-		//Debug.DrawLine(cachedTransform.position, (player.position - cachedTransform.position) * 500.0f + cachedTransform.position, Color.green);
-		//Debug.Log("X: " + player.position.x);
-		//Debug.Log(angle * Mathf.Sign(player.position.x));
-		if (angle * Mathf.Sign(player.position.x) > 10.0f) {
-			cachedTransform.Rotate(Vector3.up * Time.deltaTime * Mathf.Lerp(0.0f, speed, (angle - 10.0f)) / 5.0f);
+		Vector3 dirToPlayer = player.position - cachedTransform.position;
+		dirToPlayer.y = 0.0f;
+		//dirToPlayer = dirToPlayer.normalized;
+		float angle = Vector3.Angle(Vector3.back, dirToPlayer);
+		// Debug.DrawLine(cachedTransform.position, Vector3.back * 500.0f + cachedTransform.position, Color.white);
+		// Debug.DrawLine(cachedTransform.position, dirToPlayer * 500.0f + cachedTransform.position, Color.green);
+		// Debug.Log("X: " + player.position.x);
+		Debug.Log(angle * Mathf.Sign(player.position.x));
+		//Debug.Log(angle);
+		if (angle * Mathf.Sign(player.position.x) > 5.0f) {
+			// cachedTransform.forward = Vector3.Lerp(cachedTransform.forward, -dirToPlayer, Time.deltaTime * speed);
+			cachedTransform.Rotate(Vector3.up * Time.deltaTime * Mathf.Lerp(0.0f, speed, (angle - 5.0f)) / 5.0f);
 		}
-		if (angle * Mathf.Sign(player.position.x) < 10.0f) {
-			cachedTransform.Rotate(-Vector3.up * Time.deltaTime * Mathf.Lerp(0.0f, speed, (angle - 10.0f)) / 5.0f);
+		if (angle * Mathf.Sign(player.position.x) < -5.0f) {
+			cachedTransform.Rotate(-Vector3.up * Time.deltaTime * Mathf.Lerp(0.0f, speed, (angle - 5.0f)) / 5.0f);
 		}
 	}
 }
